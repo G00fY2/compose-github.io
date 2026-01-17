e76bcdf2fac75593be867e15e7007c627dab5d16
Thomas Wirth <thomas-w-@web.de>
2026-01-17 18:02:29 +0100

    Refactor settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,14 +3,22 @@ rootProject.name = "compose-github.io"
 dependencyResolutionManagement {
   @Suppress("UnstableApiUsage")
   repositories {
+    google {
+      @Suppress("UnstableApiUsage")
+      mavenContent {
+        includeGroupAndSubgroups("androidx")
+        includeGroupAndSubgroups("com.android")
+        includeGroupAndSubgroups("com.google")
+        includeGroupAndSubgroups("org.jetbrains")
+      }
+    }
     mavenCentral()
-    google()
   }
 }
 
 pluginManagement {
   repositories {
-    mavenCentral()
     gradlePluginPortal()
+    mavenCentral()
   }
 }
